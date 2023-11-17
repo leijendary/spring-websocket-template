1b86e8525f951958eb7e9f1069f822c5d76cc64b
leijendary <jonathanleijendekker@gmail.com>
2023-11-17 12:10:39 +0100

    Added type reference to String.toClass

--- src/main/kotlin/com/leijendary/spring/template/websocket/core/extension/StringExtension.kt
+++ src/main/kotlin/com/leijendary/spring/template/websocket/core/extension/StringExtension.kt
@@ -5,3 +5,7 @@ import com.leijendary.spring.template.websocket.core.util.BeanContainer.objectMa
 inline fun <reified T : Any> String.toClass(): T {
     return objectMapper.readValue(this, T::class.java)
 }
+
+fun <T> String.toClass(reference: TypeReference<T>): T {
+    return objectMapper.readValue(this, reference)
+}
